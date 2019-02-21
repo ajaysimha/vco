f1fd005fa1b8a930bc1f11eb04c3eb46b0c88436
ajaysimha <ajsimha@gmail.com>
2019-02-21 14:15:28 -0500

    updated template and homedir after simple test

--- scripts/0-site-settings.sh
+++ scripts/0-site-settings.sh
@@ -1,21 +1,19 @@
 
 # satellite config
-export satellite_server='pod2-satellite.cloud.practice.redhat.com'
-export satellite_server_ip='10.12.134.15'
-export activation_key='osp13-director-dev'
-export organization='pod2'
-#export pool='8a85f98c60c2c2b40160c32447481b48'
+#export satellite_server='pod2-satellite.cloud.practice.redhat.com'
+#export satellite_server_ip='10.12.134.15'
+#export activation_key='osp13-director-dev'
+export organization='core'
+export pool='8a85f99a67cdc3e701681095609a0e3a'
 
-export hostname='pod2-undercloud'
-export ip_address='10.12.134.20'
-export domain='cloud.practice.redhat.com'
-export stack_password='redhat'
+export hostname='undercloud-core'
+export ip_address='10.255.5.205'
+export domain='wwtatc.com'
+export stack_password='redhatvco'
 export osp_version='13'
 
 export stack_name='overcloud'
-export ntp_server='clock.corp.redhat.com'
-#export log_server_hostname='pod2-logmon'
-#export log_server_ip='10.12.34.8'
+export ntp_server='10.255.0.1'
 
 home=~
 rcfile=$home/$stack_name
