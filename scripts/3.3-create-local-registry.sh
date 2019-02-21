555be1fc1662ce85dfd610827477483f4b95967d
ajaysimha <ajsimha@gmail.com>
2019-02-21 14:19:41 -0500

    updated 3.3-create-local-registry.sh

--- scripts/3.3-create-local-registry.sh
+++ scripts/3.3-create-local-registry.sh
@@ -1,13 +1,15 @@
-openstack overcloud container image prepare \
-  --namespace=docker-registry.engineering.redhat.com/rhosp13 \
-  --push-destination=192.168.24.1:8787 \
-  --prefix=openstack- \
-  --tag=latest \
-  --output-env-file=/home/stack/templates/environments/overcloud-images.yaml \
-  --output-images-file /home/stack/local-registry-images.yaml \
-  -r -r {ENVIRONMENTS}/roles_data.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-sriov.yaml \
-  --set ceph_namespace=registry.access.redhat.com/rhceph \
-  --set ceph_image=rhceph-3-rhel7 \
-  --set ceph_tag=latest
+#openstack overcloud container image prepare \
+#  --namespace=registry.access.redhat.com/rhosp13 \
+#  --push-destination=192.168.86.18:8787 \
+#  --prefix=openstack- \
+#  --tag-from-label {version}-{release} \
+#  -r /home/stack/templates/environments/roles_data.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+#  -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-sriov.yaml \
+#  --set ceph_namespace=registry.access.redhat.com/rhceph \
+#  --set ceph_image=rhceph-3-rhel7 \
+#  --set ceph_tag=latest \
+#  --output-env-file=/home/stack/templates/overcloud_images.yaml \
+#  --output-images-file /home/stack/local_registry_images.yaml
+
+openstack overcloud container image prepare --namespace=registry.access.redhat.com/rhosp13 --push-destination=192.168.86.18:8787 --prefix=openstack- --tag-from-label {version}-{release} -r /home/stack/templates/environments/roles_data.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/services-docker/neutron-sriov.yaml --set ceph_namespace=registry.access.redhat.com/rhceph --set ceph_image=rhceph-3-rhel7 --set ceph_tag=latest --output-env-file=/home/stack/templates/overcloud_images.yaml --output-images-file /home/stack/local_registry_images.yaml
